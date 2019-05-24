978986e468e501abf3d8c17dc4e17cabf01605d5
Igor <gurupatikus@gmail.com>
2019-05-24 18:35:09 +0300

    angular 7 update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
   MatButtonModule, MatCheckboxModule, MatCardModule
 } from '@angular/material';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './root/app.component';
+import { LandingLayoutComponent } from './layouts/landing-layout/landing-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingLayoutComponent
   ],
   imports: [
     // angular material
@@ -23,7 +24,6 @@ import { AppComponent } from './app.component';
     //
     BrowserModule,
     FormsModule,
-    HttpModule,
     AppRoutingModule
   ],
   providers: [],
